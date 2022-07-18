06944b9028106c83f783668f22073bb90c4663df
johch3n611u <401450650@gapp.fju.edu.tw>
2022-07-18 23:58:00 +0800

    重開專案

--- BackEnd/Master/Model/Model.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Model
-{
-    public class Result<T>
-    {
-        public T Data { get; set; }
-        public string Message { get; set; }
-        public bool Success { get; set; } = true;
-    }
-}
