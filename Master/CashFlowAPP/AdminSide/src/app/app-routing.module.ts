270e5891084573005e8a4aa6664b44fa6934aa76
L1uXO330B7 <carl12331.gmail.com>
2022-08-11 15:44:24 +0800

    AdminSide Angular Material

--- Master/CashFlowAPP/AdminSide/src/app/app-routing.module.ts
+++ Master/CashFlowAPP/AdminSide/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardPageComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: '**', redirectTo: '/dashboard', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Master/CashFlowAPP/AdminSide/src/app/app.module.ts
+++ Master/CashFlowAPP/AdminSide/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { SharedMaterialModule } from './common/modules/shared/shared-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    DashboardPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
