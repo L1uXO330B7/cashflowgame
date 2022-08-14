dba46e301a286bc7e2d4ddf8e309eab24e369df5
DESKTOP-QSEF0D3\大哥 <carl12331@gmail.com>
2022-08-14 20:07:42 +0800

    admin

--- Master/CashFlowAPP/AdminSide/src/app/common/services/api.service.ts
+++ Master/CashFlowAPP/AdminSide/src/app/common/services/api.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(public _HttpClient: HttpClient) { }
+  _HttpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+    _ApiUrl = `${environment.ApiRoot}`;
+
 }
--- Master/CashFlowAPP/AdminSide/src/app/common/services/http-interceptor.service.ts
+++ Master/CashFlowAPP/AdminSide/src/app/common/services/http-interceptor.service.ts
@@ -1,9 +1,59 @@
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest,} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HttpInterceptorService {
+export class HttpInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private route:Router
+  ) {
+
+  }
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+
+
+    let NewRequest;
+    let Token = localStorage.getItem('Token');
+
+    if (Token != 'null') {
+      NewRequest = req.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + Token,
+          AccessControlAllowOrigin: '*',
+          AccessControlAllowMethods: 'GET, PUT, POST, DELETE, OPTIONS'
+        }
+      });
+    } else {
+      NewRequest = req;
+    }
+
+    return next.handle(NewRequest)
+    .pipe(
+      map((event: any) => {
+
+        // https://www.tpisoftware.com/tpu/articleDetails/1084
+
+        console.log('HttpInterceptorService event', event.body);
+        if(event.body!==undefined||null){
+          if(event.body.Success){
+        }
+        return event;
+      }}),
+      catchError((error: HttpErrorResponse) => {
+        console.log('HttpInterceptorService error', error);
+        console.log('req', req);
+        // https://stackoverflow.com/questions/68655492/throwerrorerror-is-now-deprecated-but-there-is-no-new-errorhttperrorresponse
+        return throwError(() => error);
+
+      }),
+    );
+  }
 }
+
