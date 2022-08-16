09724534c78cb03cbb76ee31892c6fda7af78d78
L1uXO330B7 <carl12331.gmail.com>
2022-08-16 15:39:20 +0800

    Table

--- Master/CashFlowAPP/AdminSide/src/app/pages/dashboard-page/dashboard-page.component.ts
+++ Master/CashFlowAPP/AdminSide/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,7 +1,6 @@
-import { ApiRequest } from '../../common/models/ApiRequest';
+
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { ApiService } from 'src/app/common/services/api.service';
+
 
 @Component({
   selector: 'app-dashboard-page',
@@ -10,18 +9,10 @@ import { ApiService } from 'src/app/common/services/api.service';
 })
 export class DashboardPageComponent implements OnInit {
 
-  constructor(private _HttpClient: HttpClient, private _ApiService: ApiService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.UsersRead();
+
   }
-  UsersRead(){
-    let Req = new ApiRequest<any>();
-    Req.Args=[];
-    this._ApiService.UsersRead(Req).subscribe((Res)=>{
-      if(Res.Success){
-        console.log("Res",Res);
-      }
-    })
-  }
+
 }
