9b91fe7e75382b5ea00e8edd13b9c0a33d293880
L1uXO330B7 <carl12331.gmail.com>
2022-08-18 09:05:23 +0800

    page problem

--- Master/CashFlowAPP/AdminSide/src/app/pages/users-table/users-table.component.ts
+++ Master/CashFlowAPP/AdminSide/src/app/pages/users-table/users-table.component.ts
@@ -1,10 +1,10 @@
+import { MatTableDataSource } from '@angular/material/table';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiRequest } from 'src/app/common/models/ApiRequest';
 import { ApiService } from 'src/app/common/services/api.service';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
+
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-users-table',
@@ -14,21 +14,46 @@ import { MatPaginator } from '@angular/material/paginator';
 export class UsersTableComponent implements OnInit {
 
   constructor(private _HttpClient: HttpClient, private _ApiService: ApiService) { }
+  @ViewChild('paginator') paginator: MatPaginator | any;
 
   ngOnInit(): void {
-    this.UsersRead();
-    this.UsersData.paginator = this.paginator;
+
+    this.UsersRead(); this.GetPages(0, 5)
+
   }
 
-  @ViewChild('paginator') paginator: MatPaginator|any;
-  UsersData:any;
-  totalCount:any;
-  UsersRead(){
+
+  ngAfterViewInit() {
+    this.UsersData.paginator = this.paginator;
+    this.paginator.page.subscribe((page: PageEvent) => {
+      this.GetPages(page.pageIndex, page.pageSize);
+    });
+  }
+
+  UsersData: any;
+  totalCount: any;
+  UsersRead() {
     let Req = new ApiRequest<any>();
-    Req.Args=[];
-    this._ApiService.UsersRead(Req).subscribe((Res)=>{
-      if(Res.Success){
-        console.log("Res",Res);
+    Req.Args = [];
+    this._ApiService.UsersRead(Req).subscribe((Res) => {
+      if (Res.Success) {
+        console.log("Res", Res);
+        this.UsersData = new MatTableDataSource(Res.Data);
+        this.totalCount = Res.Data.length;
+      }
+    })
+  }
+
+
+
+  GetPages(pageIndex: any, pageSize: any) {
+    let Req = new ApiRequest<any>();
+    Req.Args = [];
+    Req.PageIndex = pageIndex;
+    Req.PageSize = pageSize;
+    this._ApiService.UsersRead(Req).subscribe((Res) => {
+      if (Res.Success) {
+        console.log("Res", Res);
         this.UsersData = Res.Data;
         this.totalCount = Res.Data.length;
       }
