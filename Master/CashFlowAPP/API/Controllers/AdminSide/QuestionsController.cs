86a10566d543d14e5b56d7d91832af10ac66a47f
johch3n611u <401450650@gapp.fju.edu.tw>
2022-08-21 22:33:54 +0800

    問卷 v1 fix

--- Master/CashFlowAPP/API/Controllers/AdminSide/QuestionsController.cs
+++ Master/CashFlowAPP/API/Controllers/AdminSide/QuestionsController.cs
@@ -1,48 +1,48 @@
-﻿using BLL.IServices;
-using Common.Model;
-using Common.Model.AdminSide;
-using Microsoft.AspNetCore.Mvc;
+﻿
 
-namespace API.Controllers.AdminSide
-{
-    [Route("api/[controller]/[action]")]
-    [ApiController]
-    public class QuestionsController : Controller, ICrudController<List<CreateUserArgs>, List<ReadQuestionArgs>, List<UpdateUserArgs>, List<int?>>
-    {
-        private IQuestionsService<List<CreateUserArgs>, List<ReadQuestionArgs>, List<UpdateUserArgs>, List<int?>> _QuestionsService;
+        using Microsoft.AspNetCore.Mvc;
+        using Common.Model;
+        using BLL.IServices;
+        using Common.Model.AdminSide;
 
-        /// <summary>
-        /// 建構子
-        /// </summary>
-        /// <param name="IQuestionsService"></param>
-        public QuestionsController(
-            IQuestionsService<List<CreateUserArgs>, List<ReadQuestionArgs>, List<UpdateUserArgs>, List<int?>> IQuestionsService
-        )
+        namespace API.Controllers.AdminSide
         {
-            _QuestionsService = IQuestionsService;
-        }
+           
+            [Route("api/[controller]/[action]")]
+            [ApiController]
+            public class QuestionsController : Controller,ICrudController<List<CreateQuestionArgs>, List<ReadQuestionArgs>, List<UpdateQuestionArgs>, List<int?>>
+            {
+                private IQuestionsService<List<CreateQuestionArgs>, List<ReadQuestionArgs>, List<UpdateQuestionArgs>, List<int?>> _QuestionsService;
 
+                public QuestionsController(
+                    IQuestionsService<List<CreateQuestionArgs>, List<ReadQuestionArgs>, List<UpdateQuestionArgs>, List<int?>> IQuestionsService
+                )
+                {
+                    _QuestionsService = IQuestionsService;
+                }
 
-        [HttpPost]
-        public Task<ApiResponse> Create([FromBody] ApiRequest<List<CreateUserArgs>> Req)
-        {
-            throw new NotImplementedException();
-        }
-        [HttpPost]
-        public Task<ApiResponse> Delete([FromBody] ApiRequest<List<int?>> Req)
-        {
-            throw new NotImplementedException();
-        }
+                [HttpPost]
+                public async Task<ApiResponse> Create([FromBody] ApiRequest<List<CreateQuestionArgs>> Req)
+                {
+                    return await _QuestionsService.Create(Req);
+                }
 
-        [HttpPost]
-        public async Task<ApiResponse> Read([FromBody] ApiRequest<List<ReadQuestionArgs>> Req)
-        {
-            return await _QuestionsService.Read(Req);
+                [HttpPost]
+                public async Task<ApiResponse> Delete([FromBody] ApiRequest<List<int?>> Req)
+                {
+                    return await _QuestionsService.Delete(Req);
+                }
+
+                [HttpPost]
+                public async Task<ApiResponse> Read([FromBody] ApiRequest<List<ReadQuestionArgs>> Req)
+                {
+                    return await _QuestionsService.Read(Req);
+                }
+
+                [HttpPost]
+                public async Task<ApiResponse> Update([FromBody] ApiRequest<List<UpdateQuestionArgs>> Req)
+                {
+                    return await _QuestionsService.Update(Req);
+                }
+            }
         }
-        [HttpPost]
-        public Task<ApiResponse> Update([FromBody] ApiRequest<List<UpdateUserArgs>> Req)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
