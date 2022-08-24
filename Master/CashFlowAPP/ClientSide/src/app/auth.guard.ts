3145251f1ee3fdb042bdcfea167115a9ac853a38
L1uXO330B7 <carl12331.gmail.com>
2022-08-24 14:26:35 +0800

    AuthoGuard mvp okay

--- Master/CashFlowAPP/ClientSide/src/app/auth.guard.ts
+++ Master/CashFlowAPP/ClientSide/src/app/auth.guard.ts
@@ -9,7 +9,22 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.canEnter();
   }
-  
+
+  canEnter():boolean{
+    let UserName = localStorage.getItem("StrangerName");
+    if(UserName==""){
+      return false;
+    }
+    if(UserName==null){
+      return false;
+    }
+    if(UserName==undefined){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
 }
