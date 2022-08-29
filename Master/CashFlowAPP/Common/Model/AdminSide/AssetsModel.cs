1ae1ca64aea055de3b307927f991db659977566d
johch3n611u <401450650@gapp.fju.edu.tw>
2022-08-29 23:57:38 +0800

    推

--- Master/CashFlowAPP/Common/Model/AdminSide/AssetsModel.cs
+++ Master/CashFlowAPP/Common/Model/AdminSide/AssetsModel.cs
@@ -1,42 +1,42 @@
 ﻿
 
-        using DPL.EF;
-        using System;
-        using System.Collections.Generic;
-        using System.ComponentModel.DataAnnotations;
-        using System.Linq;
-        using System.Text;
-        using System.Threading.Tasks;
+using DPL.EF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-        namespace Common.Model.AdminSide
-        {
-            public class CreateAssetArgs : Asset
-            {
-                public int Id { get;set; } 
-public string Name { get;set; } 
-public decimal Value { get;set; } 
-public byte Status { get;set; } 
-public int AssetCategoryId { get;set; } 
+namespace Common.Model.AdminSide
+{
+    public class CreateAssetArgs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public byte Status { get; set; }
+        public int AssetCategoryId { get; set; }
 
-            }
+    }
 
-            public class UpdateAssetArgs : Asset
-            {
-                public int Id { get;set; } 
-public string Name { get;set; } 
-public decimal Value { get;set; } 
-public byte Status { get;set; } 
-public int AssetCategoryId { get;set; } 
+    public class UpdateAssetArgs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public byte Status { get; set; }
+        public int AssetCategoryId { get; set; }
 
-            }
+    }
 
-            /// <summary>
-            /// 查詢條件
-            /// </summary>
-            public class ReadAssetArgs
-            {
-                public string Key { get; set; } = "Id";
-                public string JsonString { get; set; } = "[1,2,3]";
-            }
-        }
+    /// <summary>
+    /// 查詢條件
+    /// </summary>
+    public class ReadAssetArgs
+    {
+        public string Key { get; set; } = "Id";
+        public string JsonString { get; set; } = "[1,2,3]";
+    }
+}
 
--- Master/CashFlowAPP/Common/Model/AdminSide/CashFlowsModel.cs
+++ Master/CashFlowAPP/Common/Model/AdminSide/CashFlowsModel.cs
@@ -1,42 +1,42 @@
 ﻿
 
-        using DPL.EF;
-        using System;
-        using System.Collections.Generic;
-        using System.ComponentModel.DataAnnotations;
-        using System.Linq;
-        using System.Text;
-        using System.Threading.Tasks;
+using DPL.EF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-        namespace Common.Model.AdminSide
-        {
-            public class CreateCashFlowArgs : CashFlow
-            {
-                public int Id { get;set; } 
-public string Name { get;set; } 
-public decimal Value { get;set; } 
-public int CashFlowCategoryId { get;set; } 
-public byte Status { get;set; } 
+namespace Common.Model.AdminSide
+{
+    public class CreateCashFlowArgs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public int CashFlowCategoryId { get; set; }
+        public byte Status { get; set; }
 
-            }
+    }
 
-            public class UpdateCashFlowArgs : CashFlow
-            {
-                public int Id { get;set; } 
-public string Name { get;set; } 
-public decimal Value { get;set; } 
-public int CashFlowCategoryId { get;set; } 
-public byte Status { get;set; } 
+    public class UpdateCashFlowArgs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public int CashFlowCategoryId { get; set; }
+        public byte Status { get; set; }
 
-            }
+    }
 
-            /// <summary>
-            /// 查詢條件
-            /// </summary>
-            public class ReadCashFlowArgs
-            {
-                public string Key { get; set; } = "Id";
-                public string JsonString { get; set; } = "[1,2,3]";
-            }
-        }
+    /// <summary>
+    /// 查詢條件
+    /// </summary>
+    public class ReadCashFlowArgs
+    {
+        public string Key { get; set; } = "Id";
+        public string JsonString { get; set; } = "[1,2,3]";
+    }
+}
 
