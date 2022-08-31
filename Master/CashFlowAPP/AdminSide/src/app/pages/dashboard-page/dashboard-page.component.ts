de432a49351c7ccf330cf4ca4b6fb9c5de5c9adc
L1uXO330B7 <carl12331.gmail.com>
2022-08-31 11:08:28 +0800

    chart.js not work

--- Master/CashFlowAPP/AdminSide/src/app/pages/dashboard-page/dashboard-page.component.ts
+++ Master/CashFlowAPP/AdminSide/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,5 +1,7 @@
+import { Router } from '@angular/router';
 
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 
 @Component({
@@ -7,12 +9,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements AfterViewInit  {
 
-  constructor() { }
-
+  constructor(private _Router:Router) { }
+  ngAfterViewInit(){
+     this.context=this.buyers.nativeElement.getContext('2d');
+    new Chart(this.context,this.buyerData);
+  }
   ngOnInit(): void {
 
   }
 
+  AdminLogout(){
+    localStorage.removeItem("UserId");
+    localStorage.removeItem("Token");
+    this._Router.navigate(['/', 'login']);
+  }
+
+  @ViewChild('buyers', { static: true }) buyers: ElementRef<HTMLCanvasElement>|any;
+  public context: CanvasRenderingContext2D | any;
+
+  buyerData:any = {
+    labels : ["January","February","March","April","May","June"],
+    datasets : [
+    {
+        fillColor : "rgba(172,194,132,0.4)",
+        strokeColor : "#ACC26D",
+        pointColor : "#fff",
+        pointStrokeColor : "#9DB86D",
+        data : [203,156,99,251,305,247]
+    }
+]
+}
 }
